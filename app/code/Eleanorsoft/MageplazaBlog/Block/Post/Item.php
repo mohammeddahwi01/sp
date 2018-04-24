a2e460e0dc94c5bde51eeb90d7c8d6f93ab98ddf
Denis Pisarenko <denis.pisarenko@eleanorsoft.com>
2018-04-24 16:58:19 +0300

    resize image

--- app/code/Eleanorsoft/MageplazaBlog/Block/Post/Item.php
+++ app/code/Eleanorsoft/MageplazaBlog/Block/Post/Item.php
@@ -2,6 +2,8 @@
 
 namespace Eleanorsoft\MageplazaBlog\Block\Post;
 
+use Eleanorsoft\MageplazaBlog\Helper\Image;
+use Mageplaza\Blog\Block\Frontend;
 use Mageplaza\Blog\Model\Post as PostItem;
 use Magento\Framework\View\Element\Template;
 use Mageplaza\Blog\Helper\Data as HelperData;
@@ -14,6 +16,7 @@ use Magento\Framework\View\Element\Template\Context;
  *
  * @package Eleanorsoft\MageplazaBlog\Block\Post
  * @author Eugene Polischuk <eugene.polischuk@eleanorsoft.com>
+ * @author Denis Pisarenko <denis.pisarenko@eleanorsoft.com>
  * @copyright Copyright (c) 2018 Eleanorsoft (https://www.eleanorsoft.com/)
  */
 class Item extends Template
@@ -24,19 +27,35 @@ class Item extends Template
     protected $helperData;
 
     /**
+     * @var Frontend
+     */
+    protected $frontend;
+
+    /**
+     * @var Image
+     */
+    protected $imageHelper;
+
+    /**
      * Item constructor.
      *
      * @param Context $context
      * @param HelperData $helperData
+     * @param Frontend $frontend
+     * @param Image $imageHelper
      * @param array $data
      * @author Eugene Polischuk <eugene.polischuk@eleanorsoft.com>
      */
     public function __construct(
         Context $context,
         HelperData $helperData,
+        Frontend $frontend,
+        Image $imageHelper,
         array $data = []
     ) {
         $this->helperData = $helperData;
+        $this->frontend = $frontend;
+        $this->imageHelper = $imageHelper;
 
         parent::__construct($context, $data);
     }
@@ -131,6 +150,25 @@ class Item extends Template
      */
     public function getDefaultImageUrl()
     {
-        return $this->getViewFileUrl('Mageplaza_Blog::media/images/mageplaza-logo-default.png');
+        return $this->frontend->getDefaultImageUrl();
     }
+
+    /**
+     * Resize Image Function
+     * @param PostItem $post
+     * @param null $width
+     * @param null $height
+     * @return string
+     */
+    public function resizeImage($post, $width = null, $height = null)
+    {
+        $image = $post->getImage();
+
+        if (!$image) {
+            return $this->getDefaultImageUrl();
+        }
+
+        return $this->imageHelper->resizeImage($image, $width, $height);
+    }
+
 }
