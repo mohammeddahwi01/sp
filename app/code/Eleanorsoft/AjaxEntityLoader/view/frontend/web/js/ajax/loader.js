726049f40758ec11af12411af924f3716ec45418
denis <den3538@gmail.com>
2018-05-31 15:32:31 +0300

    front changes

--- app/code/Eleanorsoft/AjaxEntityLoader/view/frontend/web/js/ajax/loader.js
+++ app/code/Eleanorsoft/AjaxEntityLoader/view/frontend/web/js/ajax/loader.js
@@ -1,39 +1,69 @@
-define
-(
-    [
-        'jquery',
-        'uiComponent',
-        'ko',
-        'mage/storage'
-    ],
-    function ($, Component, ko, storage)
-    {
-        'use script';
-
+define(['jquery', 'uiComponent', 'ko', 'mage/storage'], function ($, Component, ko,storage ) {
+        'use strict';
         return Component.extend({
             defaults: {
-                qty: null,
-                urlProducts: null
+                endResult: ko.observableArray([]),
+                counter: 0,
+                canMakeRequest: true,
+                canMakeRequestNow: true
             },
-
-            initialize: function () {
+            initialize: function (config) {
+                var self = this;
+                console.log(config.qty);
+                this.option = config;
                 this._super();
-                alert(this.urlProducts);
-                // this.getProducts();
-
+                $(window).scroll(function() {
+                    if (
+                        self.canMakeRequest &&
+                        self.canMakeRequestNow &&
+                        $(window).scrollTop() === ($(document).height() - $(window).height())
+                    ) {
+                        self.updateEntityLoader();
+                    }
+                });
             },
+            updateEntityLoader: function () {
+                var self = this;
+                this.getProducts().then(function () {
+                    if (!Object.keys(arguments[0]).length) {
+                        self.canMakeRequest = false;
+                        console.log('canMakeRequest then',self.canMakeRequest);
+                    }
 
+                    $.each(arguments[0], function (key,value) {
+                        self.endResult.push(value);
+                    });
+                    console.log(self.endResult());
+                }).catch(
+                    function (e) {
+                        console.log(e);
+                        console.log("error");
+                    }
+                )
+            },
             getProducts: function () {
                 var self = this;
+                self.canMakeRequestNow = false;
+                console.log('self.canMakeRequestNow getProducts',self.canMakeRequestNow)
 
-                return storage.post
-                (
-                    this.urlProducts,
-                    ''
-                ).done(function (response) {
+                return new Promise( function(resolve, reject) {
+                    self.counter++;
 
-                }).fail(function (response) {
-                    console.log(response)
+                    return storage.get(window.location.href+"&p="+self.counter+"&qty="+self.option.qty).done(
+                        function (response) {
+                            resolve(response);
+                        }
+                    ).fail(
+                        function (response) {
+                            reject(response);
+                        }
+                    ).always(
+                        function () {
+                            self.canMakeRequestNow = true;
+                            console.log('finished');
+                            console.log('self.canMakeRequestNow always',self.canMakeRequestNow)
+                        }
+                    );
                 });
             }
         });
